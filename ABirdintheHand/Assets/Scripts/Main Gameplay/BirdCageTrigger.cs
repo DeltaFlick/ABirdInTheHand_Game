ba0c52cd169a385a6a218623f67d85b741222628
DeltaFlick <toynemitchell@gmail.com>
2025-08-29 10:57:18 +0800

    Simple Jailbreak Feature

--- ABirdintheHand/Assets/Scripts/Main Gameplay/BirdCageTrigger.cs
+++ ABirdintheHand/Assets/Scripts/Main Gameplay/BirdCageTrigger.cs
@@ -6,16 +6,29 @@ using UnityEngine.SceneManagement;
 public class BirdCageTrigger : MonoBehaviour
 {
     private HashSet<GameObject> birdsInCage = new HashSet<GameObject>();
+    private Coroutine rescueRoutine;
+
+    [Header("Rescue Settings")]
+    [SerializeField] private float rescueTime = 3f;
+    [SerializeField] private Transform releasePoint;
 
     private void OnTriggerEnter(Collider other)
     {
         BirdIdentifier bird = other.GetComponent<BirdIdentifier>();
-        if (bird != null && bird.IsBeingHeld)
+        if (bird != null)
         {
-            if (!birdsInCage.Contains(other.gameObject))
+            if (bird.IsBeingHeld)
             {
-                birdsInCage.Add(other.gameObject);
-                CheckWinCondition();
+                if (!birdsInCage.Contains(other.gameObject))
+                {
+                    birdsInCage.Add(other.gameObject);
+                    CheckWinCondition();
+                }
+            }
+            else
+            {
+                if (rescueRoutine == null)
+                    rescueRoutine = StartCoroutine(RescueCountdown());
             }
         }
     }
@@ -25,11 +38,53 @@ public class BirdCageTrigger : MonoBehaviour
         BirdIdentifier bird = other.GetComponent<BirdIdentifier>();
         if (bird != null)
         {
+            if (!bird.IsBeingHeld && rescueRoutine != null)
+            {
+                StopCoroutine(rescueRoutine);
+                rescueRoutine = null;
+            }
+
             if (birdsInCage.Contains(other.gameObject))
             {
                 birdsInCage.Remove(other.gameObject);
             }
         }
+    }
+
+    private IEnumerator RescueCountdown()
+    {
+        float timer = 0f;
+        while (timer < rescueTime)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        ReleaseAllBirds();
+        rescueRoutine = null;
+    }
+
+    private void ReleaseAllBirds()
+    {
+        foreach (GameObject bird in birdsInCage)
+        {
+            if (bird != null)
+            {
+                Rigidbody rb = bird.GetComponent<Rigidbody>();
+                if (rb != null) rb.isKinematic = false;
+
+                if (releasePoint != null)
+                {
+                    bird.transform.position = releasePoint.position;
+                    bird.transform.rotation = releasePoint.rotation;
+                }
+
+                BirdIdentifier id = bird.GetComponent<BirdIdentifier>();
+                if (id != null) id.IsBeingHeld = false;
+            }
+        }
+
+        birdsInCage.Clear();
     }
 
     private void CheckWinCondition()
@@ -38,7 +93,7 @@ public class BirdCageTrigger : MonoBehaviour
 
         if (totalBirds > 0 && birdsInCage.Count == totalBirds)
         {
-            SceneManager.LoadScene("HumansWin");
+            SceneManager.LoadScene("Humans1Win");
         }
     }
 }
