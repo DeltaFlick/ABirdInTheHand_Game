dce9bc4a5d8e0b80dc507899be6cdce1569cfd86
DeltaFlick <toynemitchell@gmail.com>
2025-10-17 14:00:45 +0800

    Revert "Cleanup"
    This reverts commit 28b57d739b30a16b5170c8786410d7f2eb9432e8.

--- ABirdintheHand/Assets/ABirdInTheHand/Scripts/Main Gameplay/BreakableFalling.cs
+++ ABirdintheHand/Assets/ABirdInTheHand/Scripts/Main Gameplay/BreakableFalling.cs
@@ -10,35 +10,28 @@ public class BreakableFalling : MonoBehaviour
 {
     public float scoreAddAmount = 10;
     public float impactThreshold = 5f;
+
     public GameObject destroyedVersion;
 
     private Rigidbody rb;
     private ScoreSystem scoreSystem;
-    private bool hasBroken = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
-        GameObject controllerObj = GameObject.FindGameObjectWithTag("GameController");
-        if (controllerObj != null)
-            scoreSystem = controllerObj.GetComponent<ScoreSystem>();
+        scoreSystem = GameObject.FindGameObjectWithTag("GameController").GetComponent<ScoreSystem>();
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if (hasBroken) return;
-
         if (collision.gameObject.CompareTag("whatisGround"))
         {
             float impactVelocity = collision.relativeVelocity.magnitude;
 
             if (impactVelocity > impactThreshold)
             {
-                hasBroken = true;
-                scoreSystem?.AddScore(scoreAddAmount);
+                scoreSystem.AddScore(scoreAddAmount);
                 ForceDrop.RequestDropAll();
-
                 Instantiate(destroyedVersion, transform.position, transform.rotation);
                 Destroy(gameObject);
             }
